205adadd7cdd0149c445b0c8d613394e855ea596
ashokspro <ashokpcshannmugam@gmail.com>
2025-07-30 13:57:48 +0530

    Add missing files

--- dist/PowerE/PowerE_GUI.bat
+++ dist/PowerE/PowerE_GUI.bat
@@ -6,6 +6,6 @@ REM Get the directory of the batch file (no matter where it's run from)
 cd /d "./dist/main/"
 
 REM Run the EXE from the same directory with --settings argument and admin permission
-powershell -Command "Start-Process '.\POWER-E.exe' -ArgumentList '--settings' -Verb RunAs"
+powershell -Command "Start-Process '.\PowerE.exe' -ArgumentList '--settings' -Verb RunAs"
 
 pause
